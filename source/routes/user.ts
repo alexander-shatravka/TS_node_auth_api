1ef136cc837538d49f78661d5b9419a986e229ae
Alexander Shatravka <alexander.shatravka@gmail.com>
2021-09-22 17:57:05 +0300

    upload project

---
+++ source/routes/user.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import controller from '../controllers/user';
+import extractJWT from '../middleware/extractJWT';
+
+const router = express.Router();
+
+router.get('/validate', extractJWT, controller.validateToken);
+router.post('/register', controller.register);
+router.post('/login', controller.login);
+router.post('/logout', controller.logout);
+router.get('/get/all', controller.getAllUsers);
+
+export = router;
